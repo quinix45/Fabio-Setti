9c42fcf4a70728a636e40ebc83d0b7d08266a2c7
Fabio Setti <fabio.setti45@gmail.com>
2025-09-24 08:55:32 -0400

    Started Lab 6

--- static/PSYC6802/Qmd_files_compiler.R
+++ static/PSYC6802/Qmd_files_compiler.R
@@ -1,7 +1,8 @@
-# 
-# qmd_files <- list.files()[grep(".qmd", list.files())]
-# 
-# for(i in 1:length(qmd_files)){
-#   
-#   quarto::quarto_render(input = qmd_files[i])
-# }
+# run to compile all .qmd files in directory (used when theme updates)
+
+qmd_files <- list.files()[grep(".qmd", list.files())]
+
+for(i in 1:length(qmd_files)){
+  
+  quarto::quarto_render(input = qmd_files[i])
+}
