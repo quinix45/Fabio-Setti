f219e60267c7f1f2c8eee960bf4f372aa597ef80
Fabio Setti <fabio.setti45@gmail.com>
2026-04-23 21:31:28 -0400

    more slides updates

--- static/Presentations/Bayes_May4_2026/QR_codes.R
+++ static/Presentations/Bayes_May4_2026/QR_codes.R
@@ -1,5 +1,7 @@
 library(qrcode)
 
+## survey
+
 code <- qr_code(
   "https://docs.google.com/forms/d/e/1FAIpQLSeTi_lk3Fm8t6rUND_GYNK--Bdz51KA6ItQRFSjha2MebjGLQ/viewform?usp=header",
   ecl = "H"
@@ -8,5 +10,22 @@ code <- qr_code(
 generate_svg(
   code,
   filename = "static/Presentations/Bayes_May4_2026/Additional files/Images/SurveyQR.svg",
+  show = FALSE
+)
+
+
+## slides
+
+"https://fabio-setti.netlify.app/Presentations/Bayes_May4_2026/Main"
+
+
+code_slide <- qr_code(
+  "https://fabio-setti.netlify.app/Presentations/Bayes_May4_2026/Main",
+  ecl = "H"
+)
+
+generate_svg(
+  code_slide,
+  filename = "static/Presentations/Bayes_May4_2026/Additional files/Images/SlidesQR.svg",
   show = FALSE
 )
